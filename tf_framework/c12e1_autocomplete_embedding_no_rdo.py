c59af04a63e7bb59c4f1348991c3f858f06eb32e
Magnus Ekman <magnus@ekman.net>
2022-09-12 19:38:49 -0700

    Add alternative versions of TensorFlow autocomplete examples
    No recurrent dropout to work around hang on some platforms.

---
+++ tf_framework/c12e1_autocomplete_embedding_no_rdo.py
@@ -0,0 +1,150 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2021 NVIDIA
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text \
+    import text_to_word_sequence
+import tensorflow as tf
+import logging
+tf.get_logger().setLevel(logging.ERROR)
+
+EPOCHS = 32
+BATCH_SIZE = 256
+INPUT_FILE_NAME = '../data/frankenstein.txt'
+WINDOW_LENGTH = 40
+WINDOW_STEP = 3
+PREDICT_LENGTH = 3
+MAX_WORDS = 10000
+EMBEDDING_WIDTH = 100
+
+# Open and read file.
+file = open(INPUT_FILE_NAME, 'r', encoding='utf-8-sig')
+text = file.read()
+file.close()
+
+# Make lower case and split into individual words.
+text = text_to_word_sequence(text)
+
+# Create training examples.
+fragments = []
+targets = []
+for i in range(0, len(text) - WINDOW_LENGTH, WINDOW_STEP):
+    fragments.append(text[i: i + WINDOW_LENGTH])
+    targets.append(text[i + WINDOW_LENGTH])
+
+# Convert to indices.
+tokenizer = Tokenizer(num_words=MAX_WORDS, oov_token='UNK')
+tokenizer.fit_on_texts(text)
+fragments_indexed = tokenizer.texts_to_sequences(fragments)
+targets_indexed = tokenizer.texts_to_sequences(targets)
+
+# Convert to appropriate input and output formats.
+X = np.array(fragments_indexed, dtype=np.int64)
+y = np.zeros((len(targets_indexed), MAX_WORDS))
+for i, target_index in enumerate(targets_indexed):
+    y[i, target_index] = 1
+
+# Build and train model.
+training_model = Sequential()
+training_model.add(Embedding(
+    output_dim=EMBEDDING_WIDTH, input_dim=MAX_WORDS,
+    mask_zero=True, input_length=None))
+training_model.add(LSTM(128, return_sequences=True,
+                        dropout=0.2))
+training_model.add(LSTM(128, dropout=0.2))
+training_model.add(Dense(128, activation='relu'))
+training_model.add(Dense(MAX_WORDS, activation='softmax'))
+training_model.compile(loss='categorical_crossentropy',
+                       optimizer='adam')
+training_model.summary()
+history = training_model.fit(X, y, validation_split=0.05,
+                             batch_size=BATCH_SIZE, 
+                             epochs=EPOCHS, verbose=2, 
+                             shuffle=True)
+
+# Build stateful model used for prediction.
+inference_model = Sequential()
+inference_model.add(Embedding(
+    output_dim=EMBEDDING_WIDTH, input_dim=MAX_WORDS,
+    mask_zero=True, batch_input_shape=(1, 1)))
+inference_model.add(LSTM(128, return_sequences=True,
+                         dropout=0.2,
+                         stateful=True))
+inference_model.add(LSTM(128, dropout=0.2,
+                         stateful=True))
+inference_model.add(Dense(128, activation='relu'))
+inference_model.add(Dense(MAX_WORDS, activation='softmax'))
+weights = training_model.get_weights()
+inference_model.set_weights(weights)
+
+# Provide beginning of sentence and
+# predict next words in a greedy manner
+first_words = ['i', 'saw']
+first_words_indexed = tokenizer.texts_to_sequences(
+    first_words)
+inference_model.reset_states()
+predicted_string = ''
+# Feed initial words to the model.
+for i, word_index in enumerate(first_words_indexed):
+    x = np.zeros((1, 1), dtype=np.int64)
+    x[0][0] = word_index[0]
+    predicted_string += first_words[i]
+    predicted_string += ' '
+    y_predict = inference_model.predict(x, verbose=0)[0]
+# Predict PREDICT_LENGTH words.
+for i in range(PREDICT_LENGTH):
+    new_word_index = np.argmax(y_predict)
+    word = tokenizer.sequences_to_texts(
+        [[new_word_index]])
+    x[0][0] = new_word_index
+    predicted_string += word[0]
+    predicted_string += ' '
+    y_predict = inference_model.predict(x, verbose=0)[0]
+print(predicted_string)
+
+# Explore embedding similarities.
+embeddings = training_model.layers[0].get_weights()[0]
+lookup_words = ['the', 'saw', 'see', 'of', 'and',
+                'monster', 'frankenstein', 'read', 'eat']
+for lookup_word in lookup_words:
+    lookup_word_indexed = tokenizer.texts_to_sequences(
+        [lookup_word])
+    print('words close to:', lookup_word)
+    lookup_embedding = embeddings[lookup_word_indexed[0]]
+    word_indices = {}
+    # Calculate distances.
+    for i, embedding in enumerate(embeddings):
+        distance = np.linalg.norm(
+            embedding - lookup_embedding)
+        word_indices[distance] = i
+    # Print sorted by distance.
+    for distance in sorted(word_indices.keys())[:5]:
+        word_index = word_indices[distance]
+        word = tokenizer.sequences_to_texts([[word_index]])[0]
+        print(word + ': ', distance)
+    print('')
